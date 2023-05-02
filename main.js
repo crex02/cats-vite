2fe041ccebf837423ae174934ccfdc0abef79831
crex02 <cresta.davide02@gmail.com>
2023-05-02 19:42:05 +0200

    second

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-import './style.css'
+import './style.css';
+import './data-service';
+import {getCatsPage} from './data-service';
+
+console.log('ciao');
+
+getCatsPage().then(data => {
+  console.log(data);
+})
